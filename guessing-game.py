b0c5502c76374ae500796c3fe1741ab2664f9abe
jojovavasasa <joris.van.sande@telenet.be>
2024-11-10 20:43:51 +0100

    made the game work

---
+++ guessing-game.py
@@ -0,0 +1,17 @@
+import random
+
+tries = 0
+
+random_number = random.randint (1, 1000)
+
+while True:
+    tries = tries + 1
+    guess = int(input("Guess the number between 1 and 1000 "))
+  
+    if guess < random_number:
+        print("Too low, try again ")
+    if guess > random_number:
+        print("Too high, try again")
+    if guess == random_number:
+        print(f"Good job, the number was {random_number} and you guessed in {tries} tries, restart program to try again")
+        break
